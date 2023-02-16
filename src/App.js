607f203f64ef3c5f75d74b390197a981dc6b011b
JoanClaverol <41630699+JoanClaverol@users.noreply.github.com>
2023-02-16 18:51:30 +0100

    solved errors on fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from 'react';
-import {Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
-import useFetchData from './hooks/useFetchData';
 import Home from './views/Home';
 import Recipe from './views/Recipe';
 
@@ -11,18 +10,18 @@ function App() {
   const [recipe, setRecipe] = useState()
 
   useEffect(() => {
-    
     fetch('http://cdn.contentful.com/spaces/6glkdf3annwq/entries?access_token=Jnkx10iYp0IcHRc1oSLDugcZtEKE1N2qdz_HEnKeeRU')
       .then(res => res.json())
       .then(data => setRecipe(data.items));
-console.log(recipe)
-  }, []
-  )
+    console.log(recipe)
+  }, [])
+
+  console.log(recipe);
 
   return (
     <div className="App">
       <Header />
-   
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipe" element={<Recipe />} />
