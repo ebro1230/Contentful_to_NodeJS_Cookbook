9285ee7c9ce03afde8e8fd9bc1226d1f8c501493
ebro1230 <ebro1230@gmail.com>
2023-05-14 12:59:37 +0200

    fixing fetch

--- src/views/Home.js
+++ src/views/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
 
   useEffect(() => {
     setIsLoading(true);
-    fetch(`${process.env.REACT_APP_BACKEND_URL}api/recipes`)
+    fetch(`${process.env.REACT_APP_BACKEND_URL}api/recipes`, { mode: "cors" })
       .then((response) => {
         if (!response.ok) {
           return setError(`HTTP Status Error: ${response.status}`);
